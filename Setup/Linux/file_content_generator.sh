04b8eafd06eef764cf3232a27455fb6cce5c2d02
Christian Schweizer <christian@schweizerweb.ch>
2023-09-13 09:03:45 +0200

    linux zip

---
+++ Setup/Linux/file_content_generator.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+generate_file_section()
+{
+    echo ""
+}
+
+generate_package_section()
+{
+    echo ""
+}
+
+generate_dir_section()
+{
+    echo ""
+}
+
+get_full_path()
+{
+    echo ${1}
+}
+
+handle_fixed_files()
+{
+    echo ${1}
+}
+
+write_file()
+{
+    echo "OK"
+}
+
+build_installer()
+{
+    zip -r "${PACKAGE_NAME}.zip" ./bin/*.*
+}
